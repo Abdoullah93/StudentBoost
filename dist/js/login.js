497eceed64e0dd8a210288147be730c20539842b
Abdoullah93 <abdoullah2905@gmail.com>
2024-03-04 12:26:25 +0100

    login page front et verif connexion autorisée ou non

---
+++ dist/js/login.js
@@ -0,0 +1,9 @@
+function login_check(){
+    /*
+    0 id incorrect ne pas bouger
+    1 id correct access home page 
+    recuperer et store user_id xmlhttprequest
+    creer une session utilisateur dans le php pour recup a tout moment le userid
+    
+    */
+}
---
+++ dist/php/login.php
@@ -0,0 +1,17 @@
+<?php
+require 'database_connection_for_pomodoro.php'; 
+file_put_contents('debug.log', "DEBUT DU PROGRAMME LOGIN.PHP" . PHP_EOL, FILE_APPEND);
+$mail = $_POST['mail'];
+$pass = $_POST['pass'];
+$query="SELECT * FROM `utilisateur` WHERE mail = '$mail' AND password = '$pass';";
+$results=mysqli_query($bdd,$query);
+if ($results->num_rows > 0) {
+    $row = $results->fetch_assoc();
+    $user_id = $row['user_id'];
+    file_put_contents('debug.log', "ID CORRECT" . PHP_EOL . "user_id: " . $user_id . PHP_EOL, FILE_APPEND);
+    echo $user_id;
+} else {
+    file_put_contents('debug.log', "ID INCORRECT" . PHP_EOL, FILE_APPEND);
+    echo 0;
+}
+file_put_contents('debug.log', "FIN DU PROGRAMME LOGIN.PHP" . PHP_EOL, FILE_APPEND);
